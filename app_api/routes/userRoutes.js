ae456abc03a47606b3ec0fa5eef0d0ce755bedb6
Timothy Brunk <Timothy.Brunk@gmail.com>
2016-06-03 15:13:17 -0600

    Added controllers, routes to app_server, added index.js files

--- app_api/routes/userRoutes.js
+++ app_api/routes/userRoutes.js
@@ -5,6 +5,6 @@ var userCtrl = require('../controllers/userCtrl');
 router.get('/users', userCtrl.index);
 router.post('/users', userCtrl.create);
 router.delete('users/:user_id', userCtrl.delete);
-router.put('users/:user_id', userCtrl.update); 
+router.put('users/:user_id', userCtrl.update);
 
 module.exports = router;
