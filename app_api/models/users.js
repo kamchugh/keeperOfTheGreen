14df31d4d45664b15ebbfe9bac1a69e4b81dc96b
Timothy Brunk <Timothy.Brunk@gmail.com>
2016-06-03 11:16:50 -0600

    Models.User created

---
+++ app_api/models/users.js
@@ -0,0 +1,88 @@
+module.exports = function(sequalize, DataTypes) {
+    var User = sequalize.define("User", {
+
+        user_id : {
+          type: DataTypes.INTEGER,
+          allowNull : false,
+          unique : true
+        },
+        fname : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        lname : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        address : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        city : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        state : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        zip : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        phone : {
+          type : DataTypes.INTEGER,
+          allowNull : false,
+          unique : false
+
+        },
+        email : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        password : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        notes : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        credit : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+
+        },
+        img_url : {
+          type : DataTypes.STRING,
+          allowNull : false,
+          unique : false
+        }, {
+          classMethods: {
+            associate : function(models) {
+                User.hasMany(models.Cart)
+          }
+        }
+    });
+
+    return User;
+}
