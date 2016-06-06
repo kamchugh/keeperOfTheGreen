dfe1b01705499239e469bc60413f55b5805d65a5
Kaylee McHugh <kayleemchugh@Kaylees-MacBook-Pro.local>
2016-06-06 14:54:15 -0600

    signup and login work

--- app_api/models/product.js
+++ app_api/models/product.js
@@ -3,7 +3,9 @@ module.exports = function(sequelize, DataTypes) {
         product_id : {
             type: DataTypes.INTEGER,
             allowNull : false,
-            unique : true
+            autoIncrement: true,
+            unique : true,
+            primaryKey: true
         },
         title :  DataTypes.STRING,
 
