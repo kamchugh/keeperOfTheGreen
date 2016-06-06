855b87607914b2ad1f0d723a547661730d50d3c7
wezdez <westonbross@yahoo.com>
2016-06-06 09:27:24 -0600

    added item.js

---
+++ app_api/models/item.js
@@ -0,0 +1,21 @@
+module.exports = function(sequelize, DataTypes) {
+  var Item = sequelize.define('Item', {
+    // description: DataTypes.STRING
+    title: DataTypes.STRING
+
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Item.belongsTo(models.Cart, {
+          onDelete: 'CASCADE',
+          foreignKey: { notNull: true }
+        });
+      }
+    }
+  });
+
+  return Item;
+}
+
+
+
